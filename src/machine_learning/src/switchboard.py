39146008e3a8e92b4f855ac26b3a45728ee527e5
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2023-11-01 09:46:25 -0500

    removing unused imports

--- src/machine_learning/src/switchboard.py
+++ src/machine_learning/src/switchboard.py
@@ -3,76 +3,10 @@
 import sys
 
 sys.path.append("..")
-from processing import col_drop
-from processing import get_flag
-from processing import encode
-from processing import normalize
-from processing import get_error
-
-from data import hrrr_data
-from data import nysm_data
-
-from visuals import loss_curves
-
-from evaluate import eval_lstm
-from evaluate import fsdp
-
-from comet_ml import Optimizer
-from sklearn.feature_selection import mutual_info_classif as MIC
-from sklearn import preprocessing
-from sklearn import utils
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import functools 
-import torch
-
-from torch import nn
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
-
-
-import datetime as dt
-from datetime import date
-from datetime import datetime
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
-from tqdm import tqdm
-import re
-import emd
-import statistics as st
-from dateutil.parser import parse
-import warnings
 import os
-import xarray as xr
-import glob
-import metpy.calc as mpcalc
-from metpy.units import units
-import torch.distributed as dist
-
-from torch.optim.lr_scheduler import StepLR
-
-import torch.distributed as dist
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data.distributed import DistributedSampler
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.fully_sharded_data_parallel import (
-    CPUOffload,
-    BackwardPrefetch,
-)
-from torch.distributed.fsdp.wrap import (
-    size_based_auto_wrap_policy,
-    enable_wrap,
-    wrap,
-)
-import argparse
-import functools
+from evaluate import fsdp
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+import argparse
 
 
 if __name__ == '__main__':
