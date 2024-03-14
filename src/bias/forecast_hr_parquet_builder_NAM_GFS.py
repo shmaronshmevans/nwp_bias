Data leak fix and RNN add

src/bias/forecast_hr_parquet_builder_NAM_GFS.py
@@ -6,10 +6,12 @@ import os
 
 
 def make_dirs(year, month, day, fh, model):
-    if not os.path.exists(f"/home/aevans/ai2es/lstm/{model}/fh_{fh}/{year}/"):
-        os.makedirs(f"/home/aevans/ai2es/lstm/{model}/fh_{fh}/{year}/")
-    if not os.path.exists(f"/home/aevans/ai2es/lstm/{model}/fh_{fh}/{year}/{month}/"):
-        os.makedirs(f"/home/aevans/ai2es/lstm/{model}/fh_{fh}/{year}/{month}/")
+    if not os.path.exists(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/"):
+        os.makedirs(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/")
+    if not os.path.exists(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/{year}/"):
+        os.makedirs(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/{year}/")
+    if not os.path.exists(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/{year}/{month}/"):
+        os.makedirs(f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/{year}/{month}/")
 
 
 def main(start_date, end_date, fh, model):
@@ -62,10 +64,10 @@ def main(start_date, end_date, fh, model):
 
                 # Increment time by 6 hours
                 time_obj = datetime.strptime(my_time, "%Y-%m-%d %H:%M:%S")
-                if model == "GFS":
+                if model.upper() == "GFS":
                     my_time = time_obj + timedelta(hours=6)
-                if model == "NAM":
-                    my_time = time_obj + timedelta(hours=6)
+                if model.upper() == "NAM":
+                    my_time = time_obj + timedelta(hours=1)
 
         start_date += delta
 
@@ -73,23 +75,32 @@ def main(start_date, end_date, fh, model):
         # Reverse the order of rows and save the data to a new parquet file
         the_df = the_df.iloc[::-1]
         the_df.to_parquet(
-            f"/home/aevans/ai2es/lstm/{model}/fh_{fh}/{year}/{month}/{year}{month}{day}_{model.lower()}_fh{fh}.parquet"
+            f"/home/aevans/ai2es/lstm/{model.upper()}/fh_{fh}/{year}/{month}/{year}{month}{day}_{model.lower()}_fh{fh}.parquet"
         )
+        print(the_df)
 
 
-# Step 1: Initialize multiprocessing.Pool()
-pool = mp.Pool(mp.cpu_count())
-
-# Step 2: Use pool.apply() to execute the main function with specified arguments
-results = pool.apply(
-    main,
-    args=(
-        datetime(2018, 1, 1, 0, 0, 0),
-        datetime(2018, 12, 31, 23, 59, 59),
-        "00",
+for i in np.arange(3, 97, 3):
+    fh = str(i).zfill(3)
+    main(
+        datetime(2023, 1, 1, 0, 0, 0),
+        datetime(2023, 12, 31, 23, 59, 59),
+        fh,
         "GFS",
-    ),
-)
+        )
+# # Step 1: Initialize multiprocessing.Pool()
+#     pool = mp.Pool(mp.cpu_count())
 
-# Step 3: Close the multiprocessing pool
-pool.close()
+#     # Step 2: Use pool.apply() to execute the main function with specified arguments
+#     results = pool.apply(
+#         main,
+#         args=(
+#             datetime(2018, 1, 1, 0, 0, 0),
+#             datetime(2018, 12, 31, 23, 59, 59),
+#             fh,
+#             "GFS",
+#         ),
+#     )
+
+#     # Step 3: Close the multiprocessing pool
+#     pool.close()

src/machine_learning/random_forest/src/rnn_comet_opt.py
@@ -162,11 +162,6 @@ def main(
     torch.cuda.set_device(device)
     print(device)
     torch.manual_seed(101)
-    experiment = Experiment(
-        api_key="leAiWyR5Ck7tkdiHIT7n6QWNa",
-        project_name="hyperparameter-tuning-for-rnn",
-        workspace="shmaronshmevans",
-    )
     (
         df_train,
         df_test,
@@ -229,8 +224,7 @@ def main(
     }
 
     init_start_event.record()
-    train_loss_ls = []
-    test_loss_ls = []
+
     for ix_epoch in range(1, epochs + 1):
         train_loss = train_model(
             train_loader, model, loss_function, optimizer, device, ix_epoch
@@ -238,20 +232,15 @@ def main(
 
         test_loss = test_model(test_loader, model, loss_function, device, ix_epoch)
         print(" ")
-        train_loss_ls.append(train_loss)
-        test_loss_ls.append(test_loss)
-        # log info for comet and loss curves
-        experiment.set_epoch(ix_epoch)
+        experiment.log_metrics(hyper_params, epoch=ix_epoch)
         experiment.log_metric("test_loss", test_loss)
         experiment.log_metric("train_loss", train_loss)
-        experiment.log_metrics(hyper_params, epoch=ix_epoch)
+        
 
     init_end_event.record()
     print("Successful Experiment")
-    # Seamlessly log your Pytorch model
-    experiment.end()
     print("... completed ...")
-    return min(test_loss)
+    return test_loss
 
 
 config = {
@@ -264,10 +253,10 @@ config = {
     },
     # Declare your hyperparameters:
     "parameters": {
-        "hid_size": {"type": "integer", "min": 10, "max": 1000},
-        "n_layers": {"type": "integer", "min": 1, "max": 15},
-        "learning_rate": {"type": "float", "min": 0.0, "max": 1.0},
-        "weight_decay": {"type": "float", "min": 0.0, "max": 1.0},
+        "hid_size": {"type": "integer", "min": 50, "max": 100},
+        "n_layers": {"type": "integer", "min": 1, "max": 8},
+        "learning_rate": {"type": "float", "min": 9e-3, "max": 1e-1},
+        "weight_decay": {"type": "float", "min": 0.0, "max": 0.5},
     },
     "trials": 30,
 }
@@ -279,7 +268,7 @@ opt = Optimizer(config)
 # Finally, get experiments, and train your models:
 for experiment in opt.get_experiments(project_name="hyperparameter-tuning-for-rnn"):
     loss = main(
-        station="OLEA",
+        station="FRED",
         fh=6,
         in_size=134,
         hid_size=experiment.get_parameter("hid_size"),
@@ -288,7 +277,7 @@ for experiment in opt.get_experiments(project_name="hyperparameter-tuning-for-rn
         out_size=1,
         sequence_length=120,
         batch_size=int(10e2),
-        learning_rate=5e-3,
+        learning_rate=experiment.get_parameter("learning_rate"),
         weight_decay=experiment.get_parameter("weight_decay"),
     )
     experiment.log_metric("loss", loss)

src/machine_learning/src/evaluate/eval_single_gpu.py
@@ -60,6 +60,7 @@ def eval_model(
     features,
     device,
     station,
+    today_date
 ):
     train_eval_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=batch_size, shuffle=False
@@ -86,7 +87,7 @@ def eval_model(
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%m_%d_%Y_%H:%M:%S")
     df_out.to_parquet(
-        f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_csvs/{title}_ml_output_{station}.parquet"
+        f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_csvs/{today_date}/{station}/{title}_ml_output_{station}.parquet"
     )
 
 
@@ -282,16 +283,17 @@ def main(
         features,
         device,
         station,
+        today_date
     )
     print("Output saved!")
 
 
-main(
-    model_path="/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/20231221/lstm_v12_21_2023_16:54:04_VOOR.pth",
-    batch_size=int(10e5),
-    sequence_length=int(120),
-    station="VOOR",
-    num_layers=int(5),
-    hidden_units=int(67),
-    fh=10,
-)
+# main(
+#     model_path="/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/20231221/lstm_v12_21_2023_16:54:04_VOOR.pth",
+#     batch_size=int(10e5),
+#     sequence_length=int(120),
+#     station="VOOR",
+#     num_layers=int(5),
+#     hidden_units=int(67),
+#     fh=10,
+# )

src/machine_learning/src/evaluate/lstm_single_gpu.py
@@ -34,6 +34,7 @@ from torch.distributed.fsdp.wrap import (
 
 import pandas as pd
 import numpy as np
+import gc 
 
 from processing import col_drop
 from processing import get_flag
@@ -45,6 +46,7 @@ from evaluate import eval_single_gpu
 
 from data import hrrr_data
 from data import nysm_data
+from data import create_data_for_lstm, create_data_for_lstm_gfs 
 
 from visuals import loss_curves
 from comet_ml.integration.pytorch import log_model
@@ -69,6 +71,7 @@ class SequenceDataset(Dataset):
         sequence_length,
         forecast_hr,
         device,
+        model,
     ):
         self.dataframe = dataframe
         self.features = features
@@ -77,24 +80,38 @@ class SequenceDataset(Dataset):
         self.stations = stations
         self.forecast_hr = forecast_hr
         self.device = device
+        self.model = model
         self.y = torch.tensor(dataframe[target].values).float().to(device)
         self.X = torch.tensor(dataframe[features].values).float().to(device)
 
     def __len__(self):
         return self.X.shape[0]
 
-    def __getitem__(self, i):
-        if i >= self.sequence_length - 1:
-            i_start = i - self.sequence_length + 1
-            x = self.X[i_start : (i + 1), :]
-            x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
-                self.forecast_hr + 1, -int(len(self.stations) * 15) :
-            ]
-        else:
-            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
-            x = self.X[0 : (i + 1), :]
-            x = torch.cat((padding, x), 0)
 
+    def __getitem__(self, i):
+        if self.model != 'GFS':
+            if i >= self.sequence_length - 1:
+                i_start = i - self.sequence_length + 1
+                x = self.X[i_start : (i + 1), :]
+                x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
+                    self.forecast_hr, -int(len(self.stations) * 15) :
+                ]
+            else:
+                padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+                x = self.X[0 : (i + 1), :]
+                x = torch.cat((padding, x), 0)
+
+        else:
+            if i >= self.sequence_length - 1:
+                i_start = i - self.sequence_length + 1
+                x = self.X[i_start : (i + 1), :]
+                x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
+                    self.forecast_hr, -int(len(self.stations) * 15) :
+                ]
+            else:
+                padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+                x = self.X[0 : (i + 1), :]
+                x = torch.cat((padding, x), 0)
         return x, self.y[i]
 
 
@@ -116,7 +133,7 @@ class EarlyStopper:
         return False
 
 
-def make_dirs(today_date):
+def make_dirs(today_date, station):
     if (
         os.path.exists(
             f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/{today_date}"
@@ -129,16 +146,27 @@ def make_dirs(today_date):
         os.mkdir(
             f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_csvs/{today_date}"
         )
+    if (
+        os.path.exists(
+            f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/{today_date}/{station}"
+        )
+        == False
+    ):
+        os.mkdir(
+            f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/{today_date}/{station}"
+        )
+        os.mkdir(
+            f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_csvs/{today_date}/{station}"
+        )
 
 
-def get_time_title(station, val_loss):
-    title = f"{station}_loss_{val_loss}"
+def get_time_title(station):
     today = datetime.now()
     today_date = today.strftime("%Y%m%d")
     today_date_hr = today.strftime("%Y%m%d_%H:%M")
-    make_dirs(today_date)
+    make_dirs(today_date, station)
 
-    return title, today_date, today_date_hr
+    return today_date, today_date_hr
 
 
 class ShallowRegressionLSTM(nn.Module):
@@ -178,246 +206,6 @@ class ShallowRegressionLSTM(nn.Module):
         ).flatten()  # First dim of Hn is num_layers, which is set to 1 above.
 
         return out
-
-
-def columns_drop_hrrr(df):
-    df = df.drop(
-        columns=[
-            "level_0",
-            "index",
-            "lead time",
-            "lsm",
-            "latitude",
-            "longitude",
-            "time",
-        ]
-    )
-    return df
-
-
-def add_suffix(master_df, station):
-    cols = ["valid_time", "time"]
-    master_df = master_df.rename(
-        columns={c: c + f"_{station}" for c in master_df.columns if c not in cols}
-    )
-    return master_df
-
-
-def dataframe_wrapper(stations, df):
-    master_df = df[df["station"] == stations[0]]
-    master_df = add_suffix(master_df, stations[0])
-    for station in stations[1:]:
-        df1 = df[df["station"] == station]
-        df1 = add_suffix(df1, station)
-        master_df = master_df.merge(
-            df1, on="valid_time", suffixes=(None, f"_{station}")
-        )
-    return master_df
-
-
-def encode(data, col, max_val):
-    data["day_of_year"] = data["valid_time"].dt.dayofyear
-    sin = np.sin(2 * np.pi * data["day_of_year"] / max_val)
-    data.insert(loc=(1), column=f"{col}_sin", value=sin)
-    cos = np.cos(2 * np.pi * data["day_of_year"] / max_val)
-    data.insert(loc=(1), column=f"{col}_cos", value=cos)
-    data = data.drop(columns=["day_of_year"])
-
-    return data
-
-
-def nwp_error(target, station, df):
-    """
-    Calculate the error between NWP model data and NYSM data for a specific target variable.
-
-    Args:
-        target (str): The target variable name (e.g., 't2m' for temperature).
-        station (str): The station identifier for which data is being compared.
-        df (pd.DataFrame): The input DataFrame containing NWP and NYSM data.
-
-    Returns:
-        df (pd.DataFrame): The input DataFrame with the 'target_error' column added.
-
-    This function calculates the error between the NWP (Numerical Weather Prediction) modeldata and NYSM (New York State Mesonet) data for a specific target variable at a given station. The error is computed by subtracting the NYSM data from the NWP model data.
-    """
-
-    # Define a dictionary to map NWP variable names to NYSM variable names.
-    vars_dict = {
-        "t2m": "tair",
-        "mslma": "pres",
-        # Add more variable mappings as needed.
-    }
-
-    # Get the NYSM variable name corresponding to the target variable.
-    nysm_var = vars_dict.get(target)
-
-    # Calculate the 'target_error' by subtracting NYSM data from NWP model data.
-    target_error = df[f"{target}_{station}"] - df[f"{nysm_var}_{station}"]
-    df.insert(loc=(1), column=f"target_error", value=target_error)
-
-    return df
-
-
-def get_closest_stations(nysm_df, neighbors, target_station):
-    lats = nysm_df["lat"].unique()
-    lons = nysm_df["lon"].unique()
-
-    locations_a = pd.DataFrame()
-    locations_a["lat"] = lats
-    locations_a["lon"] = lons
-
-    for column in locations_a[["lat", "lon"]]:
-        rad = np.deg2rad(locations_a[column].values)
-        locations_a[f"{column}_rad"] = rad
-
-    locations_b = locations_a
-
-    ball = BallTree(locations_a[["lat_rad", "lon_rad"]].values, metric="haversine")
-
-    # k: The number of neighbors to return from tree
-    k = neighbors
-    # Executes a query with the second group. This will also return two arrays.
-    distances, indices = ball.query(locations_b[["lat_rad", "lon_rad"]].values, k=k)
-
-    indices_list = [indices[x][0:k] for x in range(len(indices))]
-
-    stations = nysm_df["station"].unique()
-
-    station_dict = {}
-
-    for k, _ in enumerate(stations):
-        station_dict[stations[k]] = indices_list[k]
-
-    utilize_ls = []
-    vals = station_dict.get(target_station)
-    vals
-    for v in vals:
-        x = stations[v]
-        utilize_ls.append(x)
-
-    return utilize_ls
-
-
-def which_fold(df, fold):
-    length = len(df)
-    test_len = int(length * 0.2)
-    df_train = pd.DataFrame()
-
-    for n in np.arange(0, 5):
-        if n != fold:
-            df1 = df.iloc[int(0.2 * n * length) : int(0.2 * (n + 1) * length)]
-            df_train = pd.concat([df_train, df1])
-        else:
-            df_test = df.iloc[int(0.2 * n * length) : int(0.2 * (n + 1) * length)]
-
-    return df_train, df_test
-
-
-def create_data_for_model(station, fh):
-    """
-    This function creates and processes data for a LSTM machine learning model.
-
-    Args:
-        station (str): The station identifier for which data is being processed.
-
-    Returns:
-        new_df (pandas DataFrame): A DataFrame containing processed data.
-        df_train (pandas DataFrame): A DataFrame for training the machine learning model.
-        df_test (pandas DataFrame): A DataFrame for testing the machine learning model.
-        features (list): A list of feature names.
-        forecast_lead (int): The lead time for the target variable.
-    """
-
-    # Print a message indicating the current station being processed.
-    print(f"Targeting Error for {station}")
-
-    # Load data from NYSM and HRRR sources.
-    print("-- loading data from NYSM --")
-    nysm_df = nysm_data.load_nysm_data()
-    nysm_df.reset_index(inplace=True)
-    print("-- loading data from HRRR --")
-    hrrr_df = hrrr_data.read_hrrr_data(str(fh).zfill(2))
-
-    # Rename columns for consistency.
-    nysm_df = nysm_df.rename(columns={"time_1H": "valid_time"})
-
-    # Filter NYSM data to match valid times from HRRR data
-    mytimes = hrrr_df["valid_time"].tolist()
-    nysm_df = nysm_df[nysm_df["valid_time"].isin(mytimes)]
-
-    stations = get_closest_stations(nysm_df, 4, station)
-    # stations = nysm_cats_df1["stid"].tolist()
-    # stations = ['OLEA', 'BELM', 'RAND', 'DELE']
-    hrrr_df1 = hrrr_df[hrrr_df["station"].isin(stations)]
-    nysm_df1 = nysm_df[nysm_df["station"].isin(stations)]
-
-    # format for LSTM
-    hrrr_df1 = columns_drop_hrrr(hrrr_df1)
-    master_df = dataframe_wrapper(stations, hrrr_df1)
-
-    nysm_df1 = nysm_df1.drop(
-        columns=[
-            "index",
-        ]
-    )
-    master_df2 = dataframe_wrapper(stations, nysm_df1)
-
-    # combine HRRR + NYSM data on time
-    master_df = master_df.merge(master_df2, on="valid_time", suffixes=(None, f"_xab"))
-
-    # Calculate the error using NWP data.
-    the_df = nwp_error("t2m", station, master_df)
-    valid_times = the_df["valid_time"].tolist()
-    # encode day of year to be cylcic
-    the_df = encode(the_df, "valid_time", 366)
-    # drop columns
-    the_df = the_df[the_df.columns.drop(list(the_df.filter(regex="station")))]
-    now = datetime.now()
-    print("now =", now)
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime("%m_%d_%Y_%H:%M:%S")
-
-    # Add EMD and/or Climate Indices
-    # the_df = normalize.normalize_df(the_df, valid_times)
-    the_df.to_parquet(
-        f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_csvs/{dt_string}_full_{station}.parquet"
-    )
-    new_df = the_df.drop(columns="valid_time")
-
-    # normalize data
-    cols = ["valid_time_cos", "valid_time_sin"]
-    for k, r in new_df.items():
-        if k in (cols):
-            continue
-        else:
-            means = st.mean(new_df[k])
-            stdevs = st.pstdev(new_df[k])
-            new_df[k] = (new_df[k] - means) / stdevs
-
-    features = list(new_df.columns.difference(["target_error"]))
-    lstm_df = new_df.copy()
-    target_sensor = "target_error"
-    forecast_lead = 0
-    target = f"{target_sensor}_lead_{forecast_lead}"
-    lstm_df.insert(loc=(0), column=target, value=lstm_df[target_sensor])
-    # lstm_df.insert(loc=(0), column=target, value=lstm_df[target_sensor].shift(-forecast_lead))
-    lstm_df = lstm_df.drop(columns=[target_sensor])
-    # lstm_df = lstm_df.iloc[:-forecast_lead]
-    # Split the data into training and testing sets.
-    df_train, df_test = which_fold(lstm_df, 4)
-
-    print("Test Set Fraction", len(df_test) / len(lstm_df))
-
-    # Fill missing values with zeros in the training and testing DataFrames.
-    df_train = df_train.fillna(0)
-    df_test = df_test.fillna(0)
-
-    # Print a message indicating that data processing is complete.
-    print("Data Processed")
-    print("--init model LSTM--")
-
-    return df_train, df_test, features, forecast_lead, stations, target
-
 
 def train_model(data_loader, model, loss_function, optimizer, device, epoch):
     num_batches = len(data_loader)
@@ -481,6 +269,7 @@ def main(
     epochs,
     weight_decay,
     fh,
+    model,
     sequence_length=120,
     target="target_error",
     learning_rate=5e-3,
@@ -497,6 +286,7 @@ def main(
     print(" *********")
     print("::: In Main :::")
     station = station
+    today_date, today_date_hr = get_time_title(station)
 
     (
         df_train,
@@ -505,11 +295,11 @@ def main(
         forecast_lead,
         stations,
         target,
-    ) = create_data_for_model(station, fh)
+    ) = create_data_for_lstm_gfs.create_data_for_model(station, fh, today_date) #to change which model you are matching for you need to chage which change_data_for_lstm you are pulling from 
 
     experiment = Experiment(
         api_key="leAiWyR5Ck7tkdiHIT7n6QWNa",
-        project_name="AMS_3",
+        project_name="gfs_v_hrrr",
         workspace="shmaronshmevans",
     )
     train_dataset = SequenceDataset(
@@ -520,6 +310,7 @@ def main(
         sequence_length=sequence_length,
         forecast_hr=fh,
         device=device,
+        model = model
     )
     test_dataset = SequenceDataset(
         df_test,
@@ -529,6 +320,7 @@ def main(
         sequence_length=sequence_length,
         forecast_hr=fh,
         device=device,
+        model = model
     )
 
     train_kwargs = {"batch_size": batch_size, "pin_memory": False, "shuffle": True}
@@ -576,6 +368,7 @@ def main(
     train_loss_ls = []
     test_loss_ls = []
     for ix_epoch in range(1, epochs + 1):
+        gc.collect()
         train_loss = train_model(
             train_loader, model, loss_function, optimizer, device, ix_epoch
         )
@@ -589,9 +382,9 @@ def main(
         experiment.log_metric("test_loss", test_loss)
         experiment.log_metric("train_loss", train_loss)
         experiment.log_metrics(hyper_params, epoch=ix_epoch)
-        if early_stopper.early_stop(test_loss):
-            print(f"Early stopping at epoch {ix_epoch}")
-            break
+        # if early_stopper.early_stop(test_loss):
+        #     print(f"Early stopping at epoch {ix_epoch}")
+        #     break
 
     init_end_event.record()
 
@@ -602,15 +395,16 @@ def main(
         # dd/mm/YY H:M:S
         dt_string = now.strftime("%m_%d_%Y_%H:%M:%S")
         states = model.state_dict()
-        title, today_date, today_date_hr = get_time_title(station, min(test_loss_ls))
+        title = f"{station}_loss_{min(test_loss_ls)}"
         torch.save(
             states,
-            f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/{today_date}/lstm_v{dt_string}_{station}.pth",
+            f"/home/aevans/nwp_bias/src/machine_learning/data/lstm_eval_vis/{today_date}/{station}/lstm_v{dt_string}_{station}.pth",
         )
         loss_curves.loss_curves(
             train_loss_ls, test_loss_ls, title, today_date, dt_string, rank=0
         )
 
+        # make sure main is commented when you run or the first run will do whatever station is listed in main
         eval_single_gpu.eval_model(
             train_dataset,
             df_train,
@@ -623,22 +417,24 @@ def main(
             features,
             device,
             station,
+            today_date
         )
 
     print("Successful Experiment")
     # Seamlessly log your Pytorch model
-    log_model(experiment, model, model_name="v6")
+    log_model(experiment, model, model_name="gfs_v_hrrr")
     experiment.end()
     print("... completed ...")
 
 
 # main(
-#     batch_size=int(10e3),
-#     station='BKLN',
+#     batch_size=int(10e2),
+#     station='BUFF',
 #     num_layers=5,
-#     epochs=20,
+#     epochs=100,
 #     weight_decay=0,
-#     fh=18
+#     fh=3, 
+#     model='GFS'
 # )
 
 # # first iteration to target Brooklyn
@@ -654,24 +450,25 @@ def main(
 #     )
 
 
-# # second iteration for experiment
-# nysm_clim = pd.read_csv("/home/aevans/nwp_bias/src/landtype/data/nysm.csv")
-# clim_divs = nysm_clim["climate_division_name"].unique()
+# second iteration for experiment
+nysm_clim = pd.read_csv("/home/aevans/nwp_bias/src/landtype/data/nysm.csv")
+clim_divs = nysm_clim["climate_division_name"].unique()
 
-# for c in clim_divs:
-#     print(c)
-#     df = nysm_clim[nysm_clim["climate_division_name"] == c]
-#     temp = df["stid"].unique()
-#     station = random.sample(sorted(temp), 1)
-#     for n, _ in enumerate(station):
-#         print(station[n])
-#         for f in np.arange(2,19,2):
-#             print("FH", f)
-#             main(
-#                 batch_size=int(10e3),
-#                 station=station[n],
-#                 num_layers=5,
-#                 epochs=100,
-#                 weight_decay=0,
-#                 fh=f
-#             )
+for c in clim_divs:
+    print(c)
+    df = nysm_clim[nysm_clim["climate_division_name"] == c]
+    temp = df["stid"].unique()
+    station = random.sample(sorted(temp), 1)
+    for n, _ in enumerate(station):
+        print(station[n])
+        for f in np.arange(3,31,3):
+            print("FH", f)
+            main(
+                batch_size=int(10e3),
+                station=station[n],
+                num_layers=5,
+                epochs=100,
+                weight_decay=0,
+                fh=f,
+                model='GFS'
+            )

src/machine_learning/src/t2m_hrrr_fh2_western_plateau_opt.py
@@ -47,6 +47,7 @@ from evaluate import eval_single_gpu
 
 from data import hrrr_data
 from data import nysm_data
+from data import create_data_for_lstm, create_data_for_lstm_gfs
 
 from visuals import loss_curves
 from datetime import datetime
@@ -70,6 +71,7 @@ class SequenceDataset(Dataset):
         sequence_length,
         forecast_hr,
         device,
+        model,
     ):
         self.dataframe = dataframe
         self.features = features
@@ -78,24 +80,38 @@ class SequenceDataset(Dataset):
         self.stations = stations
         self.forecast_hr = forecast_hr
         self.device = device
+        self.model = model
         self.y = torch.tensor(dataframe[target].values).float().to(device)
         self.X = torch.tensor(dataframe[features].values).float().to(device)
 
     def __len__(self):
         return self.X.shape[0]
 
-    def __getitem__(self, i):
-        if i >= self.sequence_length - 1:
-            i_start = i - self.sequence_length + 1
-            x = self.X[i_start : (i + 1), :]
-            x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
-                self.forecast_hr + 1, -int(len(self.stations) * 15) :
-            ]
-        else:
-            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
-            x = self.X[0 : (i + 1), :]
-            x = torch.cat((padding, x), 0)
 
+    def __getitem__(self, i):
+        if self.model != 'GFS':
+            if i >= self.sequence_length - 1:
+                i_start = i - self.sequence_length + 1
+                x = self.X[i_start : (i + 1), :]
+                x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
+                    self.forecast_hr, -int(len(self.stations) * 15) :
+                ]
+            else:
+                padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+                x = self.X[0 : (i + 1), :]
+                x = torch.cat((padding, x), 0)
+
+        else:
+            if i >= self.sequence_length - 1:
+                i_start = i - self.sequence_length + 1
+                x = self.X[i_start : (i + 1), :]
+                x[: self.forecast_hr, -int(len(self.stations) * 15) :] = x[
+                    self.forecast_hr, -int(len(self.stations) * 15) :
+                ]
+            else:
+                padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+                x = self.X[0 : (i + 1), :]
+                x = torch.cat((padding, x), 0)
         return x, self.y[i]
 
 
@@ -132,14 +148,13 @@ def make_dirs(today_date):
         )
 
 
-def get_time_title(station, val_loss):
-    title = f"{station}_loss_{val_loss}"
+def get_time_title(station):
     today = datetime.now()
     today_date = today.strftime("%Y%m%d")
     today_date_hr = today.strftime("%Y%m%d_%H:%M")
     make_dirs(today_date)
 
-    return title, today_date, today_date_hr
+    return today_date, today_date_hr
 
 
 class ShallowRegressionLSTM(nn.Module):
@@ -498,6 +513,7 @@ def main(
     fh,
     delta,
     learning_rate,
+    model,
     sequence_length=120,
     target="target_error",
     save_model=False,
@@ -513,6 +529,7 @@ def main(
     print(" *********")
     print("::: In Main :::")
     station = station
+    today_date, today_date_hr = get_time_title(station)
 
     (
         df_train,
@@ -521,7 +538,7 @@ def main(
         forecast_lead,
         stations,
         target,
-    ) = create_data_for_model(station, fh)
+    ) = create_data_for_lstm_gfs.create_data_for_model(station, fh, today_date) #to change which model you are matching for you need to chage which change_data_for_lstm you are pulling from 
     train_dataset = SequenceDataset(
         df_train,
         target=target,
@@ -530,6 +547,7 @@ def main(
         sequence_length=sequence_length,
         forecast_hr=fh,
         device=device,
+        model = model
     )
     test_dataset = SequenceDataset(
         df_test,
@@ -539,6 +557,7 @@ def main(
         sequence_length=sequence_length,
         forecast_hr=fh,
         device=device,
+        model = model
     )
 
     train_kwargs = {"batch_size": batch_size, "pin_memory": False, "shuffle": True}
@@ -583,8 +602,7 @@ def main(
     early_stopper = EarlyStopper(15)
 
     init_start_event.record()
-    train_loss_ls = []
-    test_loss_ls = []
+
     for ix_epoch in range(1, epochs + 1):
         train_loss = train_model(
             train_loader, model, loss_function, optimizer, device, ix_epoch
@@ -637,16 +655,17 @@ print("!!! begin optimizer !!!")
 opt = Optimizer(config)
 
 # Finally, get experiments, and train your models:
-for experiment in opt.get_experiments(project_name="hyperparameter-tuning-for-lstm"):
+for experiment in opt.get_experiments(project_name="hyperparameter-tuning-for-lstm-gfs"):
     loss = main(
         batch_size=120,
         station="OLEA",
         num_layers=experiment.get_parameter("num_layers"),
         epochs=100,
         weight_decay=experiment.get_parameter("weight_decay"),
-        fh=4,
+        fh=3,
         delta=experiment.get_parameter("delta"),
         learning_rate=experiment.get_parameter("learning_rate"),
+        model = 'GFS'
     )
 
     experiment.log_metric("loss", loss)